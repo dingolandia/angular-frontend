adc3749398e3db2078e2fc3ed778cf723b8a2fb6
Anderson M C Pedro <85419952+dingolandia@users.noreply.github.com>
2023-10-31 12:33:31 -0300

    Shopping component

---
+++ src/app/pages/ecommerce/components/shopping/shopping.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { FULL_URL } from 'src/app/constants/enpoints';
+import { IItem } from 'src/app/models/shop.item.interface';
+import { currencyFormat } from 'src/app/utils/formatter';
+
+@Component({
+  selector: 'app-shopping',
+  templateUrl: './shopping.component.html',
+  styleUrls: ['./shopping.component.scss']
+})
+export class ShoppingComponent {
+  @Input() preco: number = 0;
+  @Input() itemTitle: string = 'Título';
+  @Input() itemShop: IItem | undefined;
+
+  currencyFormat = currencyFormat;
+  fullUrl = FULL_URL;
+  ngOnInit(): void {
+
+  }
+
+}
