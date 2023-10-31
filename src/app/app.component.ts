037acf798a62b2406a849a4db2e568404153dac3
Anderson M C Pedro <85419952+dingolandia@users.noreply.github.com>
2023-10-31 12:20:54 -0300

    Setting up the startup component and module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+//app.component.ts
+
+import { Component, inject } from '@angular/core';
+import { Isettings } from './models/settings.interface';
+import { SettingsService } from './services/settings.service';
+import { ImainMenuItems } from './models/menu.interface';
+import { MenuService } from './services/menu.service';
+import { Router } from '@angular/router';
+import { ContentComponent } from './components/content/content/content.component';
+import { P404Component } from './pages/p404/p404.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-head [settings]="appSettings" [menuItems]="menuItems"></app-head>
+    <app-contents-wrap [settings]="appSettings"></app-contents-wrap>
+    <app-footer [settings]="appSettings"></app-footer>
+  `,
 })
 export class AppComponent {
   title = 'casadinamo';
+  appSettings: Isettings | undefined;
+  menuItems: ImainMenuItems[] = [];
+
+  private settingsService = inject(SettingsService);
+  private menuService = inject(MenuService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.settingsService.getSettings().subscribe((data) => {
+      if (data !== undefined) {
+        this.appSettings = data;
+      }
+    });
+    this.menuService.getMenu().subscribe((data) => {
+      if (data !== undefined) {
+        this.menuItems = data;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,73 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+//app.module.ts
 
-import { AppRoutingModule } from './app-routing.module';
+import {
+  APP_INITIALIZER,
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeadModule } from './modules/head/head.module';
+import { ContentModule } from './modules/content/content.module';
+import { FooterModule } from './modules/footer/footer.module';
 import { AppComponent } from './app.component';
+import { SettingsService } from './services/settings.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductsComponent } from './pages/ecommerce/pages/products/products.component';
+import { StoreComponent } from './pages/ecommerce/pages/store/store.component';
+import { NavigationBarComponent } from './pages/ecommerce/components/navigation-bar/navigation-bar.component';
+import { ProductDetailComponent } from './pages/ecommerce/components/product-detail/product-detail.component';
+import { SearchBarComponent } from './pages/ecommerce/components/search-bar/search-bar.component';
+import { ShoppingComponent } from './pages/ecommerce/components/shopping/shopping.component';
+import { P404Component } from './pages/p404/p404.component';
+import { RouteInitService } from './services/route-init.service';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+export function initializeApp(routerInitService: RouteInitService) {
+  return (): Promise<any> => {
+    return routerInitService.initAsync();
+  };
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    CatalogComponent,
+    ProductsComponent,
+    StoreComponent,
+    NavigationBarComponent,
+    ProductDetailComponent,
+    SearchBarComponent,
+    ShoppingComponent,
+    P404Component,
   ],
   imports: [
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    HeadModule,
+    ContentModule,
+    FooterModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    RouteInitService,
+    SettingsService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      multi: true,
+      deps: [RouteInitService],
+    },
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
