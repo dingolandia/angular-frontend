e640cdeec0f0e9bec22b0634b57ffb5c306aadb4
Anderson M C Pedro <85419952+dingolandia@users.noreply.github.com>
2023-10-31 12:22:59 -0300

    title-bar component

---
+++ src/app/components/content/title-bar/title-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-title-bar',
+  templateUrl: './title-bar.component.html',
+  styleUrls: ['./title-bar.component.scss'],
+})
+export class TitleBarComponent {
+  @Input() title: string | undefined;
+}
