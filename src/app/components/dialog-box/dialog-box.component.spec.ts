ea0000eaa0a0e95b6f6b6fb6898dc50028c72ad5
Anderson M C Pedro <85419952+dingolandia@users.noreply.github.com>
2023-11-16 02:39:06 -0300

    Update

--- src/app/components/dialog-box/dialog-box.component.spec.ts
+++ src/app/components/dialog-box/dialog-box.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogBoxComponent } from './dialog-box.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('DialogBoxComponent', () => {
   let component: DialogBoxComponent;
@@ -8,7 +9,8 @@ describe('DialogBoxComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [DialogBoxComponent]
+      declarations: [DialogBoxComponent],
+      imports: [MatDialogModule],
     });
     fixture = TestBed.createComponent(DialogBoxComponent);
     component = fixture.componentInstance;
