c695e0201beddd8e91c3858132e3a3a1bc281feb
Anderson M C Pedro <85419952+dingolandia@users.noreply.github.com>
2023-11-16 02:42:50 -0300

    Still beta but with no errors

--- src/app/pages/ecommerce/components/shopping/shopping.component.spec.ts
+++ src/app/pages/ecommerce/components/shopping/shopping.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShoppingComponent } from './shopping.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('ShoppingComponent', () => {
+describe('Pages/Ecommerce/Components/ShoppingComponent', () => {
   let component: ShoppingComponent;
   let fixture: ComponentFixture<ShoppingComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ShoppingComponent]
+      declarations: [ShoppingComponent],
+      imports: [RouterTestingModule],
     });
     fixture = TestBed.createComponent(ShoppingComponent);
     component = fixture.componentInstance;
